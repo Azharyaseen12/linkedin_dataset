01cbeb644cf0f6f1dae0ec68498ac778c27cbc0f
azharyaseen <86882951+azharyaseen@users.noreply.github.com>
2024-04-10 03:24:17 +0500

    issue solved

--- app/templatetags/custom_filters.py
+++ app/templatetags/custom_filters.py
@@ -3,5 +3,12 @@ from django import template
 register = template.Library()
 
 @register.filter
-def is_currently_employed(occupation, company_name):
-    return company_name.lower() in occupation.lower()
+def is_currently_employed(occupation, company_names):
+    # Split the company names by comma and strip whitespace from each name
+    company_names_list = [name.strip() for name in company_names.split(',')]
+    
+    # Check if any of the company names exists in the occupation
+    for company_name in company_names_list:
+        if company_name.lower() in occupation.lower():
+            return "Employed"  # Return "Employed" if any company name is found in the occupation
+    return "Not Employed"
