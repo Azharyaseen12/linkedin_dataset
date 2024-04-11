fdd3e8270315af792c65637e4a7ea2d94b099054
azharyaseen <86882951+azharyaseen@users.noreply.github.com>
2024-04-11 23:06:45 +0500

    error soled

--- app/templatetags/custom_filters.py
+++ app/templatetags/custom_filters.py
@@ -5,10 +5,13 @@ register = template.Library()
 @register.filter
 def is_currently_employed(occupation, company_names):
     # Split the company names by comma and strip whitespace from each name
-    company_names_list = [name.strip() for name in company_names.split(',')]
-    
-    # Check if any of the company names exists in the occupation
-    for company_name in company_names_list:
-        if company_name.lower() in occupation.lower():
-            return "Employed"  # Return "Employed" if any company name is found in the occupation
-    return "Not Employed"
+    try:
+        company_names_list = [name.strip() for name in company_names.split(',')]
+        
+        # Check if any of the company names exists in the occupation
+        for company_name in company_names_list:
+            if company_name.lower() in occupation.lower():
+                return "Employed"  # Return "Employed" if any company name is found in the occupation
+        return "Not Employed"
+    except:
+        return "Not confirmed"
